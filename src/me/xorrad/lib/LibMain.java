64186567413f788577e2de22badae13d31039daa
Xorrad <monsieurs.aymeric@gmail.com>
2024-06-25 13:38:59 +0200

    fix: add static plugin instance to LibMain

--- src/me/xorrad/lib/LibMain.java
+++ src/me/xorrad/lib/LibMain.java
@@ -3,7 +3,8 @@ package me.xorrad.lib;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LibMain {
+    public static JavaPlugin instance = null;
     public static JavaPlugin getInstance() {
-        return null;
+        return instance;
     }
 }
